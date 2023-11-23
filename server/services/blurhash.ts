98b319bf2eb7b0cb22abb6d4e89eb01a80cb3f60
Mayoreta <loviana.mayoreta@alteacare.com>
2023-11-23 14:58:16 +0700

    feat: enhance blurhash method

--- server/services/blurhash.ts
+++ server/services/blurhash.ts
@@ -1,15 +1,52 @@
-import { Strapi } from '@strapi/strapi'
-import { encode } from 'blurhash'
-import { loadImage, createCanvas } from 'canvas'
+import { Strapi } from "@strapi/strapi";
+import axios from "axios";
+import * as sharp from "sharp";
+import { encode } from "blurhash";
+
+const loadImage = async (url: string): Promise<Buffer> => {
+  const { data } = await axios.get(url, {
+    responseType: "arraybuffer",
+  });
+
+  const buffer = Buffer.from(data, "utf-8");
+
+  return buffer;
+};
+
+const encodeImage = async (buffer: Buffer): Promise<string> => {
+  const image = sharp(buffer);
+  const { width, height } = await image.metadata();
+
+  let w, h;
+  w = h = 32;
+  if (width && height) {
+    if (width > height) w = Math.round((w * width) / height);
+    else h = Math.round((h * height) / width);
+  }
+
+  const { data, info } = await image
+    .raw()
+    .ensureAlpha()
+    .resize(w, h, { fit: "fill" })
+    .toBuffer({ resolveWithObject: true });
+
+  let componentX = 3;
+  let componentY = Math.round((3 * info.height) / info.width);
+
+  if (info.width > info.height) {
+    componentX = Math.round((3 * info.width) / info.height);
+    componentY = 3;
+  }
+
+  const buf = new Uint8ClampedArray(data);
+  return encode(buf, info.width, info.height, componentX, componentY);
+};
 
 export default ({ strapi }: { strapi: Strapi }) => ({
   async generateBlurhash(imageUrl: string) {
-    const image = await loadImage(imageUrl)
-    const canvas = createCanvas(image.width, image.height)
-    const context = canvas.getContext('2d')
-    context.drawImage(image, 0, 0)
-    const imageData = context.getImageData(0, 0, image.width, image.height)
+    const image = await loadImage(imageUrl);
+    const blurhash = await encodeImage(image);
 
-    return encode(imageData.data, imageData.width, imageData.height, 5, 3)
+    return blurhash;
   },
-})
+});
